24dd8e3896251fc06127b4874a2c925fe54a2c7c
Lisset Fones <38204070+kenlizzey@users.noreply.github.com>
2018-05-14 10:08:25 -0500

    Preguntas de cuestioanrio
    Se agregaron las preguntas del cuestionario

--- ProyectoConsultaFI/Cuestionario.cs
+++ ProyectoConsultaFI/Cuestionario.cs
@@ -34,26 +34,26 @@ namespace ProyectoConsultaFI
             //Se Crea la colección de 20 preguntas con respuestas. 
             Preguntas = new ArrayList();
 
-            Preguntas.Add(new PreguntasDelCuestionaro("Pregunta1", "RA", "RB", "RCCC", "RB", '1')); //Pregunta 1
-            Preguntas.Add(new PreguntasDelCuestionaro("Pregunta2", "RA2", "RB", "RCCC", "RB", '1'));
-            Preguntas.Add(new PreguntasDelCuestionaro("Pregunta3", "RA3", "RB", "RCCC", "RB", '1'));
-            Preguntas.Add(new PreguntasDelCuestionaro("Pregunta4", "RA4", "RB", "RCCC", "RB", '1'));
-            Preguntas.Add(new PreguntasDelCuestionaro("Pregunta5", "RA5", "RB", "RCCC", "RB", '1'));
-            Preguntas.Add(new PreguntasDelCuestionaro("Pregunta6", "RA6", "RB", "RCCC", "RB",'2'));
-            Preguntas.Add(new PreguntasDelCuestionaro("Pregunta7", "RA7", "RB", "RCCC", "RB",'2'));
-            Preguntas.Add(new PreguntasDelCuestionaro("Pregunta8", "RA8", "RB", "RCCC", "RB",'2'));
-            Preguntas.Add(new PreguntasDelCuestionaro("Pregunta9", "RA9", "RB", "RCCC", "RB",'2'));
-            Preguntas.Add(new PreguntasDelCuestionaro("Pregunta10", "RA10", "RB", "RCCC", "RB",'2')); //Pregunta 10
-            Preguntas.Add(new PreguntasDelCuestionaro("Pregunta11", "RA11", "RB", "RCCC", "RB", '3'));
-            Preguntas.Add(new PreguntasDelCuestionaro("Pregunta12", "RA12", "RB", "RCCC", "RB",'3'));
-            Preguntas.Add(new PreguntasDelCuestionaro("Pregunta13", "RA13", "RB", "RCCC", "RB",'3'));
-            Preguntas.Add(new PreguntasDelCuestionaro("Pregunta14", "RA14", "RB", "RCCC", "RB", '3'));
-            Preguntas.Add(new PreguntasDelCuestionaro("Pregunta15", "RA15", "RB", "RCCC", "RB", '3'));
-            Preguntas.Add(new PreguntasDelCuestionaro("Pregunta16", "RA16", "RB", "RCCC", "RB", '4'));
-            Preguntas.Add(new PreguntasDelCuestionaro("Pregunta17", "RA17", "RB", "RCCC", "RB", '4'));
-            Preguntas.Add(new PreguntasDelCuestionaro("Pregunta18", "RA18", "RB", "RCCC", "RB", '4'));
-            Preguntas.Add(new PreguntasDelCuestionaro("Pregunta19", "RA19", "RB", "RCCC", "RB", '4'));
-            Preguntas.Add(new PreguntasDelCuestionaro("Pregunta20", "RA20", "RB", "RCCC", "RB", '4')); //Pregunta 20
+            Preguntas.Add(new PreguntasDelCuestionaro("¿Cuál es la raíz del siguiente polinomio? \n P(X)=X^3-3X^2+2", "2,3-sqrt(2)", "1,1+sqrt(3), 1-sqrt(3)", "-1+sqrt(3),1,-1", "-2,1+sqrt(2)", '1')); //Pregunta 1
+            Preguntas.Add(new PreguntasDelCuestionaro(" Factoriza el siguiente polinomio aplicando las reglas notables: X^2-4 ", "x=1,x=2", "x=-1,X=-2", "x=1,x=2", "x=-1,X=2", '1'));
+            Preguntas.Add(new PreguntasDelCuestionaro(" Descompón en factores este polinomio y encuentra sus raíces \n 6X^4-5x^3-23^2+20-4", "(x+2)(x-2)(2x-1)(3x-1) \n las raíces: -2,2,1/2,1/3", "(x+2)(x-3)(2x-1)(4x-1) \n las raíces: -2,3,1/2,1/4", "(x+2)(x-2)(2x+1)(3x+1) \n las raíces: -2,-2,1/2,-1/3", "RB", '1'));
+            Preguntas.Add(new PreguntasDelCuestionaro(" ¿Cuál es la raíz del siguiente polinomio? \n X^2-4x+4", "Raiz doble x=2", "x=-2", "Raíz doble =-1/2", "X=-1/2", '1'));
+            Preguntas.Add(new PreguntasDelCuestionaro(" Encuentra la raíz del polinomio \n 2x^4+15x^3+31x^2+12x", "x(x+4)(x+3)(2x+1)", "2x(x-4)(x+3)(1/2x+1)", "x(x+4)(x+3)(2x-1)", "(x+4)(x+3)(2x-1)", '1'));
+            Preguntas.Add(new PreguntasDelCuestionaro(" De las siguientes respuestas cúales ¿cuáles son las raíces del polinomio P(x)=x^2-5x-14?", "x=2,7", "x=3,6", "x=1,7", "2x=1,7", '2'));
+            Preguntas.Add(new PreguntasDelCuestionaro(" Por factores notables factoriza: \n P(X)=x^2+10x+25 ", "(X+5)^2", "(X-2)^2", "(X-8)^2", "(-X+2)^2", '2'));
+            Preguntas.Add(new PreguntasDelCuestionaro(" Encuentra los factores del polinomio:\n P(X)=3x^3-8x^2-20x+16 ", "(x+2)(x-4)(3x-2)", "(x+1)(3x-4)(x-2)", "(x+2)(x-4)(3x+2)", "(x+2)(x+4)(2x+2)", '2'));
+            Preguntas.Add(new PreguntasDelCuestionaro(" A partir de la siguiente factorización, encuentre el polinomio \n (X-1)(X-2)(X-3) ", "X^4-9X^2+4X+12", "X^3-9X^2+4X+15", "X^4-11X^2-4X+12", "X^4-9X^2-8X+12", '2'));
+            Preguntas.Add(new PreguntasDelCuestionaro(" ¿Cuál es la raíz del siguiente polinomio? \n 2x^3-3x^2-9x+10 ", "X=1,-2,10/4 ", "X=1/2,-1,1/4", "X=1,-2", "X=0,-2,10/4", '2')); //Pregunta 10
+            Preguntas.Add(new PreguntasDelCuestionaro(" A partir de la siguiente factorización, encuentre el polinomio \n (X+1)(X-2)^2(X-3)", "X^3-2X^2-5X+6", "X^3-2X^2-5X-16", "2X^3-2X^2+5X+6", "X^3+2X^2-5X", '3'));
+            Preguntas.Add(new PreguntasDelCuestionaro(" Descompón en factores este polinomio  \n  X^2+11X+24 ", "(X+3)(X+8)", "(X-3)(X+8)", "(X+3)(X-8)", "(X+3)(X+8)X", '3'));
+            Preguntas.Add(new PreguntasDelCuestionaro(" Descompón en factores este polinomio  \n 3^3-8X^2-20X+16", "(X+2)(X-4)(3X-2)", "(X+2)(-X-4)(3X+2)", "X(X+2)(X-4)(3X-2)", "-X(X+2)(X+4)(3X-2)", '3'));
+            Preguntas.Add(new PreguntasDelCuestionaro(" ¿Cuál es el polinomio que tiene como raíces  \n x=1,x=2 ", "X^2+4 ", "-X^3-4 ", "2X^2+4 ", "1/2X^2-4 ", '3'));
+            Preguntas.Add(new PreguntasDelCuestionaro(" Por factores notables factoriza: \n X^3-9^2+27X-27", "(X-3)^3", "(X-3)^2", "(-X-3)^3", "(X+3)^3", '3'));
+            Preguntas.Add(new PreguntasDelCuestionaro(" Encuentra la raíz de x^2-2x+1 ", "Raíz doble=1", "Raíz doble=1y -1", "Raíz doble=1/2", "Raíz doble=-1/2", '4'));
+            Preguntas.Add(new PreguntasDelCuestionaro(" A partir de la siguiente factorización encuentra el polinomio \n (x-1)^2(x-3)", "x^4-9^2+4x+12", "2x^4-9^2+4x+12", "x^4+9^2+4x+113", "x^4-9^2-4x+12", '4'));
+            Preguntas.Add(new PreguntasDelCuestionaro(" ¿Cuál es el polinomio que tiene como raíces  \n x=1,1,3 ?", "x^4-9^2+4x+12", "x^4+4x+12", "x^4-9^2+4x-12", "-9^2+4x+12", '4'));
+            Preguntas.Add(new PreguntasDelCuestionaro(" A partir de la siguiente factorización encuentra el polinomio  \n (x-2)(x-7)", "P(x)=x^2-5x-14", "P(x)=3x^2-5x-14", "P(x)=x^3-5x-14", "P(x)=x^2+5x+14", '4')); //Respuesta1
+            Preguntas.Add(new PreguntasDelCuestionaro(" A partir de la siguiente factorización encuentra el polinomio \n (x+2)(x-4)(3x-2) ", "P(X)=3x^3-8x^2-20x+16 ", "P(X)=x^3-8x^2-20x-16 ", "P(X)=3x^3+10x^2-20x+16 ", "P(X)=x^3-8x^2+16 ", '4')); //Pregunta 20
         }
 
         public Cuestionario()
