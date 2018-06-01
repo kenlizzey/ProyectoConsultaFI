29eed8177df5e3ff17e014fd13eb2b55df48d443
Lisset Fones <38204070+kenlizzey@users.noreply.github.com>
2018-06-01 02:22:17 -0500

    repaso cosas
    ya puse más cosas

--- ProyectoConsultaFI/Repaso.cs
+++ ProyectoConsultaFI/Repaso.cs
@@ -1,20 +1,41 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Windows.Forms;
-
+using System.IO;
+using System.Diagnostics;
 namespace ProyectoConsultaFI
 {
     public partial class Repaso : Form
     {
-       
-        public Repaso()
-        {
-            
-            InitializeComponent();
-        }
+		string rutaCarpeta = Application.StartupPath + @"\Repaso"; //Ruta donde van a estar los archivos
 
-        private void Repaso_FormClosing(object sender, FormClosingEventArgs e)
+
+		public Repaso()
+        {
+            InitializeComponent();
+			CrearCarpeta();
+		}
+
+
+
+
+
+
+
+		private void CrearCarpeta()
+		{
+			if (Directory.Exists(rutaCarpeta))
+			{
+				//VERIFICA QUE EXISTA LA CARPETA
+			}
+			else
+			{
+				Directory.CreateDirectory(rutaCarpeta);
+			}
+
+		}
+		private void Repaso_FormClosing(object sender, FormClosingEventArgs e)
         {
             Program.consultaFI.Show();
         }
@@ -24,12 +45,35 @@ namespace ProyectoConsultaFI
             this.Close();
         }
 
+		
 
-       
+		private void linkLabel1_MouseDoubleClick(object sender, MouseEventArgs e)
+		{
+			Process proceso = new Process(); //Se va a encargar de abrir el archivo de inmediato
+			proceso.StartInfo.FileName = rutaCarpeta + @"\definición.pdf";
+			proceso.Start();
+		}
 
+		private void linkLabel2_MouseDoubleClick(object sender, MouseEventArgs e)
+		{
+			Process proceso = new Process(); //Se va a encargar de abrir el archivo de inmediato
+			proceso.StartInfo.FileName = rutaCarpeta + @"\divisivilidad.pdf";
+			proceso.Start();
 
+		}
 
+		private void linkLabel3_MouseDoubleClick(object sender, MouseEventArgs e)
+		{
+			Process proceso = new Process(); //Se va a encargar de abrir el archivo de inmediato
+			proceso.StartInfo.FileName = rutaCarpeta + @"\raícesde.pdf";
+			proceso.Start();
+		}
 
-
-    }
+		private void linkLabel4_MouseDoubleClick(object sender, MouseEventArgs e)
+		{
+			Process proceso = new Process(); //Se va a encargar de abrir el archivo de inmediato
+			proceso.StartInfo.FileName = rutaCarpeta + @"\DESCARTES.pdf";
+			proceso.Start();
+		}
+	}
 }
